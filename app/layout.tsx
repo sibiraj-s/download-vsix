3786d384a2a82cf454b805fccdbdea4b922ec83d
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2025-07-24 10:22:27 +0530

    Fix rendering fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
